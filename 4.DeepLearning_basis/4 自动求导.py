67469699ef007012eb5fc5638b4ab44af65b9233
sxq12320 <3383615693@qq.com>
2025-08-21 23:01:06 +0800

    学习 5自动求导

---
+++ 4.DeepLearning_basis/4 自动求导.py
@@ -0,0 +1,39 @@
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2 as cv
+import math
+
+x = torch.arange(4.0) #定义向量x
+x.requires_grad_(True) #开始存储梯度
+x.grad #访问梯度
+
+y = 2*torch.dot(x,x) #定义函数y = 2 xT x
+y.backward() #求导、反向传递
+
+Text = x.grad ==4*x
+
+x.grad.zero_() #清除之前的梯度缓存
+y = x.sum() #重新建立一个新的函数
+#print(y)
+y.backward() #求导、反向传递
+GRAD_x = x.grad #访问梯度
+#print(GRAD_x)
+
+x.grad.zero_() #清除之前的梯度缓存
+y=x*x
+# 等价于 y.backward(torch.ones(len(x)))
+y.sum().backward() #求导、反向传递
+GRAD_x = x.grad
+# print(GRAD_x)
+
+x.grad.zero_()
+y = x* x #对应元素相乘
+print(y)
+u = y.detach() #u就相当于一个常量 并不是变量
+z = u * x
+z.sum().backward() #求导、反向传递
+GRAD_x = x.grad
+Test = x.grad == u #检验
+# print(GRAD_x)
+# print(Test)
--- 4.DeepLearning_basis/class1.py
+++
@@ -1,7 +0,0 @@
-import torch
-import matplotlib.pyplot as plt
-import numpy as np
-import math
-
-x = torch.arange(12)
-print(x)
